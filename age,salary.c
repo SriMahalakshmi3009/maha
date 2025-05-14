f36466aaade71fd63cf2581e1b44dfd1294af0e1
SriMahalakshmi3009 <psrimahalakshmi3009@gmail.com>
2025-05-14 11:08:59 +0530

    Add files via upload

---
+++ age,salary.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("enter your age:");
+    scanf("%d",&age);
+    printf("age:%d",age);
+    printf("\nenter your salary:");
+    float salary;
+    scanf("%f",&salary);
+    printf("salary:%f",salary);
+    char name[20];
+    scanf("%s",&name);
+    printf("name:%s",name);
+}
---
+++ celtofah.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a=30;
+    float x=(a*1.8)+32;
+    printf("%f",x);
+
+    return 0;
+}
