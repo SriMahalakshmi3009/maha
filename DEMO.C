d5b9d8c45fbbd1b413e2647fe7d014258b2f2429
SriMahalakshmi3009 <psrimahalakshmi3009@gmail.com>
2025-05-14 11:21:54 +0530

    Add files via upload

---
+++ DEMO.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,add;
+     a=2;
+     b=5;
+     c=1;
+     add=a+b+c;
+    printf("\n add of %d and %d and %d three numbers is:%d\n",a,b,c,add);
+    return 0;
+}
