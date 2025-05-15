fb19c4b9118600694c740d44d6f0acc1cca8611f
SriMahalakshmi3009 <psrimahalakshmi3009@gmail.com>
2025-05-15 10:36:40 +0530

    Add files via upload

---
+++ for.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int x;
+    for(int x=1;100<1;x++){
+        printf("\n%d",x);
+        return 0;
+    }
+}
---
+++ formultiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int x=2,y=10;
+    for(int i=5;i<=x;i++){
+        printf("%d*%d=%d\n",i,y,i*y);
+        return 0;
+    }
+
+}
