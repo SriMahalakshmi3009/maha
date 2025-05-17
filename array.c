49c35a8fc803c7b6d75733e226b871e3e96f940b
SriMahalakshmi3009 <psrimahalakshmi3009@gmail.com>
2025-05-17 12:16:12 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -1,8 +1,5 @@
 #include<stdio.h>
-int main(){
-    char arr[20]="coding";
-    char x[6]={'h','a','p','p','y'};
-    printf("%c",x[4]);
-    printf("%s\n",arr);
-    printf("%s",x);
-}
+ int main(){
+     int arr[4]={1,2,3,4};
+     printf("%d",arr[0]);
+ }
